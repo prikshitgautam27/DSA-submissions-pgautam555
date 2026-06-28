0eb19d3aee7b4b143a72451fb1dbfbb2ecedefb1
Prikshit Gautam <135122621+prikshitgautam27@users.noreply.github.com>
2026-06-28 13:16:04 +0530

    Add: max-area-of-island - submission-0

---
+++ Data Structures & Algorithms/max-area-of-island/submission-0.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    void dfs(int row, int col, vector<vector<int>>& grid,
+             vector<vector<int>>& vis, int n, int m, int& count) {
+        
+        vis[row][col] = 1;
+        count++;
+
+        int delrow[4] = {-1, 0, 1, 0};
+        int delcol[4] = {0, 1, 0, -1};
+
+        for(int i = 0; i < 4; i++) {
+            int nrow = row + delrow[i];
+            int ncol = col + delcol[i];
+
+            if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m &&
+               grid[nrow][ncol] == 1 && !vis[nrow][ncol]) {
+                dfs(nrow, ncol, grid, vis, n, m, count);
+            }
+        }
+    }
+
+    int maxAreaOfIsland(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+        vector<vector<int>> vis(n, vector<int>(m, 0));
+        int maxLength = 0;
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                if(grid[i][j] == 1 && !vis[i][j]) {
+                    int count = 0;
+                    dfs(i, j, grid, vis, n, m, count);
+                    maxLength = max(maxLength, count);
+                }
+            }
+        }
+        return maxLength;
+    }
+};
